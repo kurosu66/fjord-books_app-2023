3e9cfebe1f10e3a973fcfa7a5613913c6bb7ceaf
kurosu <kurosu.66@gmail.com>
2024-01-16 20:25:54 +0900

    booksのシステムテスト実装

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -1,45 +1,59 @@
 # frozen_string_literal: true
 
 require 'application_system_test_case'
+require 'test_helper'
 
 class BooksTest < ApplicationSystemTestCase
+  fixtures :users,:reports,:books
+
   setup do
-    @book = books(:one)
+    @book = books(:harry_potter_book)
+
+    visit root_url
+    fill_in 'Eメール', with: 'alice@example.com'
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログイン'
   end
 
   test 'visiting the index' do
     visit books_url
-    assert_selector 'h1', text: 'Books'
+    assert_selector 'h1', text: '本の一覧'
   end
 
   test 'should create book' do
     visit books_url
-    click_on 'New book'
+    click_on '本の新規作成'
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Create Book'
+    fill_in 'タイトル', with: @book.title
+    fill_in 'メモ', with: @book.memo
+    fill_in '著者', with: @book.author
+    click_on '登録する'
 
-    assert_text 'Book was successfully created'
-    click_on 'Back'
+    assert_text '本が作成されました。'
+    assert_text 'ハリー・ポッター'
+    assert_text '1990年代のイギリスを舞台に、魔法使いの少年ハリー・ポッターの学校生活や、強大な闇の魔法使いヴォルデモートとの因縁と戦いを描いた物語。'
+    assert_text 'J・K・ローリング'
   end
 
   test 'should update Book' do
     visit book_url(@book)
-    click_on 'Edit this book', match: :first
+    click_on 'この本を編集', match: :first
 
-    fill_in 'Memo', with: @book.memo
-    fill_in 'Title', with: @book.title
-    click_on 'Update Book'
+    fill_in 'タイトル', with: 'ナルニア国物語'
+    fill_in 'メモ', with: 'イギリスの作家、C・S・ルイスの全7巻からなる子供向け小説シリーズ。'
+    fill_in '著者', with: 'C・S・ルイス'
+    click_on '更新する'
 
-    assert_text 'Book was successfully updated'
-    click_on 'Back'
+    assert_text '本が更新されました。'
+    assert_text 'ナルニア国物語'
+    assert_text 'イギリスの作家、C・S・ルイスの全7巻からなる子供向け小説シリーズ。'
+    assert_text 'C・S・ルイス'
   end
 
   test 'should destroy Book' do
     visit book_url(@book)
-    click_on 'Destroy this book', match: :first
+    click_on 'この本を削除', match: :first
 
-    assert_text 'Book was successfully destroyed'
+    assert_text '本が削除されました。'
   end
 end
