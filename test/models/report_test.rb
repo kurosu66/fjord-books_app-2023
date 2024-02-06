d30c09fcfa2e2b720372e47a39a2204eaa3f44c9
kurosu <kurosu.66@gmail.com>
2024-02-06 23:06:00 +0900

    モデルテストの実装

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,32 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users,:reports
+
+  test '#editable?' do
+    report = reports(:alice_report)
+    target_user = report.user
+    assert report.editable?(target_user)
+  end
+
+  test "#created_on" do
+    report = reports(:alice_report)
+    assert_equal report.created_at.to_date, report.created_on
+  end
+
+  test "#save_mentions" do
+    alice = users(:alice)
+    alice_report = alice.reports.create!(title: "alice_report", content: "自分が聴いだのは近頃途中でおもにないうでし。")
+
+    bob = users(:bob)
+    bob_report = bob.reports.create!(title: "bob_report", content: "昨日こちらの日報を読みました。http://localhost:3000/reports/#{alice_report.id}")
+
+    assert bob_report.mentioning_reports.find(alice_report.id)
+
+    alice_another_report = alice.reports.create!(title: "alice_another_report", content: "alice_another_report")
+    bob_report.update!(title: "updated_bob_report", content: "日報を更新します。http://localhost:3000/reports/#{alice_another_report.id}")
+
+    assert bob_report.mentioning_reports.find(alice_another_report.id)
+    assert_not bob_report.mentioning_reports.find_by(id: alice_report.id)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#name_or_email should return email when name is blank' do
+    user = User.new(email: 'test@test.com', name: '')
+    assert_equal 'test@test.com', user.name_or_email
+  end
+
+  test '#name_or_email should return name when name is presence' do
+    user = User.new(email: 'test@test.com', name: 'alice')
+    assert_equal 'alice', user.name_or_email
+  end
 end
