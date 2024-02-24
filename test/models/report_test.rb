0673a5e1b658f4e708ca8be585dd98f2070e485d
kurosu <kurosu.66@gmail.com>
2024-02-24 16:23:15 +0900

    report_test修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -5,9 +5,10 @@ require 'test_helper'
 class ReportTest < ActiveSupport::TestCase
 
   test '#editable?' do
-    report = reports(:alice_report)
-    target_user = report.user
-    assert report.editable?(target_user)
+    alice_report = reports(:alice_report)
+
+    assert alice_report.editable?(users(:alice))
+    assert_not alice_report.editable?(users(:bob))
   end
 
   test '#created_on' do
@@ -22,12 +23,12 @@ class ReportTest < ActiveSupport::TestCase
     bob = users(:bob)
     bob_report = bob.reports.create!(title: 'bob_report', content: "昨日こちらの日報を読みました。http://localhost:3000/reports/#{alice_report.id}")
 
-    assert bob_report.mentioning_reports.find(alice_report.id)
+    assert_equal [alice_report], bob_report.mentioning_reports
 
     alice_another_report = alice.reports.create!(title: 'alice_another_report', content: 'alice_another_report')
     bob_report.update!(title: 'updated_bob_report', content: "日報を更新します。http://localhost:3000/reports/#{alice_another_report.id}")
 
-    assert bob_report.mentioning_reports.find(alice_another_report.id)
-    assert_not bob_report.mentioning_reports.find_by(id: alice_report.id)
+    assert_equal [alice_another_report], bob_report.mentioning_reports.reload
+    assert_not_equal [alice_report], bob_report.mentioning_reports.reload
   end
 end
